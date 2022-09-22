e9c7a68d31a12024f272892748e4fb78939fa4bf
Georges Haidar <ghaidar0@gmail.com>
2022-09-22 16:10:49 +0100

    fix psql

--- Makefile
+++ Makefile
@@ -8,4 +8,4 @@ stop-postgres:
 
 .PHONY: psql
 psql:
-	@docker run --link listen-notify-demo -it --rm postgres:14 psql -h listen-notify-demo -U postgres -p 5432 -w
+	@./scripts/psql.sh
--- scripts/psql.sh
+++ scripts/psql.sh
@@ -2,10 +2,8 @@
 
 set -euf
 
-cmd="psql -h listen-notify-demo -U postgres -p 5432 -w"
-
 docker run \
   --link listen-notify-demo \
   -it --rm \
   postgres:14 \
-  "${cmd}"
+  psql -h listen-notify-demo -U postgres -p 5432 -w
