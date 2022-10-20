74b183299ee58b92eeb59b16ac78d2f0e780ccec
Georges Haidar <ghaidar0@gmail.com>
2022-10-20 13:08:09 +0100

    add observability components

--- cmd/benthos/main.go
+++ cmd/benthos/main.go
@@ -6,6 +6,8 @@ import (
 	"github.com/benthosdev/benthos/v4/public/service"
 
 	_ "github.com/benthosdev/benthos/v4/public/components/io"
+	_ "github.com/benthosdev/benthos/v4/public/components/jaeger"
+	_ "github.com/benthosdev/benthos/v4/public/components/prometheus"
 
 	_ "github.com/disintegrator/benthos-pglisten/internal/postgres"
 )
